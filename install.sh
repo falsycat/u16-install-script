071e56be277baab3e359e1a7e190051a0192c51f
falsycat <me@falsy.cat>
2022-04-26 12:59:19 +0900

    fix an issue that step number is not displayed properly

--- install.sh
+++ install.sh
@@ -6,20 +6,21 @@ MAX_STEP=4
 step=0
 
 step=$((step+1))
-echo ==== STEP $step/$max_step: INSTALL PYTHON3
+echo ==== STEP $step/$MAX_STEP: INSTALL PYTHON3
 sudo apt install python3
 
 step=$((step+1))
-echo ==== STEP $step/$max_step: DOWNLOAD VSCODE
+echo ==== STEP $step/$MAX_STEP: DOWNLOAD VSCODE
 curl -L -o vscode.deb "$VSCODE_URL"
 
 step=$((step+1))
-echo ==== STEP $step/$max_step: INSTALL VSCODE
+echo ==== STEP $step/$MAX_STEP: INSTALL VSCODE
 sudo dpkg -i vscode.deb
 rm vscode.deb
 
 step=$((step+1))
-echo ==== STEP $step/$max_step: SETUP VSCODE
+echo ==== STEP $step/$MAX_STEP: SETUP VSCODE
 code  \
+  --force  \
   --install-extension ms-python.python  \
   --install-extension MS-CEINTL.vscode-language-pack-ja
